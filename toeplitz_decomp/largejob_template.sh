c98e98c02bfdbea23f348a4fe3ec57333d183617
Steven Ufkes <sufkes@bgqdev-fen1.scinet.p7.local>
2017-06-15 13:52:18 -0400

    Fixed large job script

--- toeplitz_decomp/largejob_template.sh
+++ toeplitz_decomp/largejob_template.sh
@@ -22,12 +22,12 @@ m=8
 p=4							# VISAL SAYS: Can set to m/4, m/2, m, 2m. Fastest when set to m/2 or m/4.
 pad=1						# 0 for no padding; 1 for padding.
 
-nodes = 64					# Nonfunctional -- only for presentation purposes.
+nodes=64					# Nonfunctional -- only for presentation purposes.
 NP=8						# Number of MPI processes. Must be set to 2n for this code. NP <= (RPN * bg_size)
 RPN=8						# Number of MPI processes per node = 1,2,4,8,16,32,64. RPN <= NP
 export OMP_NUM_THREADS=8	# Number of OpenMP threads per MPI process = 1,2,4,8,16,32,64. (RPN * OMP_NUM_THREADS ) <= 64 = threads per node
 
-sourcedir = /scratch/a/aparamek/sufkes/scintillometry/toeplitz_decomp # Directory of code.
+sourcedir=/scratch/a/aparamek/sufkes/scintillometry/toeplitz_decomp # Directory of code.
 
 source /scratch/s/scinet/nolta/venv-numpy/setup
 module purge
@@ -35,11 +35,11 @@ module unload mpich2/xl
 module load binutils/2.23 bgqgcc/4.8.1 mpich2/gcc-4.8.1 
 module load python/2.7.3
 
-cd ${sourcedir}
+cd $sourcedir
 
 echo "----------------------"
 echo "STARTING in directory $PWD"
 date
 echo "n ${n}, m ${m}, bg ${nodes}, np ${NP}, rpn ${RPN}, omp ${OMP_NUM_THREADS}"
-time runjob --np ${NP} --ranks-per-node=${RPN} --envs HOME=$HOME LD_LIBRARY_PATH=/scinet/bgq/Libraries/HDF5-1.8.12/mpich2-gcc4.8.1//lib:/scinet/bgq/Libraries/fftw-3.3.4-gcc4.8.1/lib:$LD_LIBRARY_PATH PYTHONPATH=/scinet/bgq/tools/Python/python2.7.3-20131205/lib/python2.7/site-packages/ : /scratch/s/scinet/nolta/venv-numpy/bin/python ${sourcedir}/run_real_new.py ${method} ${offsetn} ${offsetm} ${n} ${m} ${p} ${pad}
+time runjob --np ${NP} --ranks-per-node=${RPN} --envs HOME=$HOME LD_LIBRARY_PATH=/scinet/bgq/Libraries/HDF5-1.8.12/mpich2-gcc4.8.1//lib:/scinet/bgq/Libraries/fftw-3.3.4-gcc4.8.1/lib:$LD_LIBRARY_PATH PYTHONPATH=/scinet/bgq/tools/Python/python2.7.3-20131205/lib/python2.7/site-packages/ : /scratch/s/scinet/nolta/venv-numpy/bin/python $sourcedir/run_real_new.py ${method} ${offsetn} ${offsetm} ${n} ${m} ${p} ${pad}
 echo "ENDED"
