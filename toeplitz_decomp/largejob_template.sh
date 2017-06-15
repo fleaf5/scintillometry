2c1fe6b157a0ece7cc06aeff31e909ac3dc087f2
Steven Ufkes <you@example.com>
2017-06-15 12:55:44 -0400

    updated large job script

--- toeplitz_decomp/largejob_template.sh
+++ toeplitz_decomp/largejob_template.sh
@@ -19,7 +19,7 @@ offsetn=0
 offsetm=0
 n=4
 m=8
-p=2							# VISAL SAYS: Can set to m/4, m/2, m, 2m. Fastest when set to m/2 or m/4.
+p=4							# VISAL SAYS: Can set to m/4, m/2, m, 2m. Fastest when set to m/2 or m/4.
 pad=1						# 0 for no padding; 1 for padding.
 
 nodes = 64					# Nonfunctional -- only for presentation purposes.
@@ -39,5 +39,5 @@ echo "----------------------"
 echo "STARTING in directory $PWD"
 date
 echo "n ${n}, m ${m}, bg ${nodes}, np ${NP}, rpn ${RPN}, omp ${OMP_NUM_THREADS}"
-time runjob --np ${NP} --ranks-per-node=${RPN} --envs HOME=$HOME LD_LIBRARY_PATH=/scinet/bgq/Libraries/HDF5-1.8.12/mpich2-gcc4.8.1//lib:/scinet/bgq/Libraries/fftw-3.3.4-gcc4.8.1/lib:$LD_LIBRARY_PATH PYTHONPATH=/scinet/bgq/tools/Python/python2.7.3-20131205/lib/python2.7/site-packages/ : /scratch/s/scinet/nolta/venv-numpy/bin/python run_real_new.py ${method} ${offsetn} ${offsetm} ${n} ${m} ${p} ${pad}
+time runjob --np ${NP} --ranks-per-node=${RPN} --envs HOME=$HOME LD_LIBRARY_PATH=/scinet/bgq/Libraries/HDF5-1.8.12/mpich2-gcc4.8.1//lib:/scinet/bgq/Libraries/fftw-3.3.4-gcc4.8.1/lib:$LD_LIBRARY_PATH PYTHONPATH=/scinet/bgq/tools/Python/python2.7.3-20131205/lib/python2.7/site-packages/ : /scratch/s/scinet/nolta/venv-numpy/bin/python /scratch/a/aparamek/sufkes/scintillometry/toeplitz_decomp/run_real_new.py ${method} ${offsetn} ${offsetm} ${n} ${m} ${p} ${pad}
 echo "ENDED"
