871d14386525f038e5af8b22ec7a5c207cc082e7
Steven Ufkes <you@example.com>
2017-07-23 11:00:20 -0400

    Cleaned up job scripts.

--- toeplitz_decomp/largejob_cprofile_template.sh
+++ toeplitz_decomp/largejob_cprofile_template.sh
@@ -4,8 +4,8 @@
 # @ comment            = "n=4, m=8, zero-padded"
 # @ error              = $(job_name).$(Host).$(jobid).err
 # @ output             = $(job_name).$(Host).$(jobid).out
-# @ bg_size            = 256
-# @ wall_clock_limit   = 23:59:59
+# @ bg_size            = 64
+# @ wall_clock_limit   = 0:30:00
 # @ bg_connectivity    = Torus
 # @ queue 
 
@@ -17,23 +17,22 @@
 method=yty2					# Scheme of decomposition. yty2 is the method described in Nilou's report.
 offsetn=0
 offsetm=0
-n=1024
-m=1024
-p=512							# VISAL SAYS: Can set to m/4, m/2, m, 2m. Fastest when set to m/2 or m/4.
+n=4
+m=8
+p=4							# VISAL SAYS: Can set to m/4, m/2, m, 2m. Fastest when set to m/2 or m/4.
 pad=1						# 0 for no padding; 1 for padding.
 
-nodes=256					# Nonfunctional -- only for presentation purposes.
-NP=2048						# Number of MPI processes. Must be set to 2n for this code. NP <= (RPN * bg_size)
+nodes=64					# Nonfunctional -- only for presentation purposes.
+NP=8						# Number of MPI processes. Must be set to 2n for this code. NP <= (RPN * bg_size)
 RPN=8						# Number of MPI processes per node = 1,2,4,8,16,32,64. RPN <= NP
-export OMP_NUM_THREADS=8	# Number of OpenMP threads per MPI process = 1,2,4,8,16,32,64. (RPN * OMP_NUM_THREADS ) <= 64 = threads per node
+OMP=1           # Number of OpenMP threads per MPI process = 1,2,4,8,16,32,64. (RPN * OMP_NUM_THREADS ) <= 64 = threads per node
 
 sourcedir=/scratch/a/aparamek/sufkes/scintillometry/toeplitz_decomp # Directory of code.
 
 source /scratch/s/scinet/nolta/venv-numpy/setup
-module purge
+module unload bgqgcc/4.4.6
 module unload mpich2/xl
-module load binutils/2.23 bgqgcc/4.8.1 mpich2/gcc-4.8.1 
-module load python/2.7.3
+module load binutils/2.23 bgqgcc/4.8.1 mpich2/gcc-4.8.1
 
 cd $sourcedir
 
@@ -41,5 +40,5 @@ echo "----------------------"
 echo "STARTING in directory $PWD"
 date
 echo "n ${n}, m ${m}, bg ${nodes}, np ${NP}, rpn ${RPN}, omp ${OMP_NUM_THREADS}"
-time runjob --np ${NP} --ranks-per-node=${RPN} --envs HOME=$HOME LD_LIBRARY_PATH=/scinet/bgq/Libraries/HDF5-1.8.12/mpich2-gcc4.8.1//lib:/scinet/bgq/Libraries/fftw-3.3.4-gcc4.8.1/lib:$LD_LIBRARY_PATH PYTHONPATH=/scinet/bgq/tools/Python/python2.7.3-20131205/lib/python2.7/site-packages/ : /scratch/s/scinet/nolta/venv-numpy/bin/python2.7 $sourcedir/run_real_profile.py ${method} ${offsetn} ${offsetm} ${n} ${m} ${p} ${pad}
+time runjob --np ${NP} --ranks-per-node=${RPN} --envs OMP_NUM_THREADS=${OMP} HOME=$HOME LD_LIBRARY_PATH=/scinet/bgq/Libraries/HDF5-1.8.12/mpich2-gcc4.8.1//lib:/scinet/bgq/Libraries/fftw-3.3.4-gcc4.8.1/lib:$LD_LIBRARY_PATH PYTHONPATH=/scinet/bgq/tools/Python/python2.7.3-20131205/lib/python2.7/site-packages/ : /scratch/s/scinet/nolta/venv-numpy/bin/python2.7 $sourcedir/run_real_profile.py ${method} ${offsetn} ${offsetm} ${n} ${m} ${p} ${pad}
 echo "ENDED"
--- toeplitz_decomp/largejob_template.sh
+++ toeplitz_decomp/largejob_template.sh
@@ -25,15 +25,14 @@ pad=1						# 0 for no padding; 1 for padding.
 nodes=64					# Nonfunctional -- only for presentation purposes.
 NP=8						# Number of MPI processes. Must be set to 2n for this code. NP <= (RPN * bg_size)
 RPN=8						# Number of MPI processes per node = 1,2,4,8,16,32,64. RPN <= NP
-export OMP_NUM_THREADS=8	# Number of OpenMP threads per MPI process = 1,2,4,8,16,32,64. (RPN * OMP_NUM_THREADS ) <= 64 = threads per node
+OMP=1           # Number of OpenMP threads per MPI process = 1,2,4,8,16,32,64. (RPN * OMP_NUM_THREADS ) <= 64 = threads per node
 
 sourcedir=/scratch/a/aparamek/sufkes/scintillometry/toeplitz_decomp # Directory of code.
 
 source /scratch/s/scinet/nolta/venv-numpy/setup
-module purge
+module unload bgqgcc/4.4.6
 module unload mpich2/xl
-module load binutils/2.23 bgqgcc/4.8.1 mpich2/gcc-4.8.1 
-module load python/2.7.3
+module load binutils/2.23 bgqgcc/4.8.1 mpich2/gcc-4.8.1
 
 cd $sourcedir
 
@@ -41,5 +40,5 @@ echo "----------------------"
 echo "STARTING in directory $PWD"
 date
 echo "n ${n}, m ${m}, bg ${nodes}, np ${NP}, rpn ${RPN}, omp ${OMP_NUM_THREADS}"
-time runjob --np ${NP} --ranks-per-node=${RPN} --envs HOME=$HOME LD_LIBRARY_PATH=/scinet/bgq/Libraries/HDF5-1.8.12/mpich2-gcc4.8.1//lib:/scinet/bgq/Libraries/fftw-3.3.4-gcc4.8.1/lib:$LD_LIBRARY_PATH PYTHONPATH=/scinet/bgq/tools/Python/python2.7.3-20131205/lib/python2.7/site-packages/ : /scratch/s/scinet/nolta/venv-numpy/bin/python2.7 $sourcedir/run_real_new.py ${method} ${offsetn} ${offsetm} ${n} ${m} ${p} ${pad}
+time runjob --np ${NP} --ranks-per-node=${RPN} --envs OMP_NUM_THREADS=${OMP} HOME=$HOME LD_LIBRARY_PATH=/scinet/bgq/Libraries/HDF5-1.8.12/mpich2-gcc4.8.1//lib:/scinet/bgq/Libraries/fftw-3.3.4-gcc4.8.1/lib:$LD_LIBRARY_PATH PYTHONPATH=/scinet/bgq/tools/Python/python2.7.3-20131205/lib/python2.7/site-packages/ : /scratch/s/scinet/nolta/venv-numpy/bin/python2.7 $sourcedir/run_real_new.py ${method} ${offsetn} ${offsetm} ${n} ${m} ${p} ${pad}
 echo "ENDED"
