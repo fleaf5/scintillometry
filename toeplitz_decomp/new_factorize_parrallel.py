05a4e30f1d0c24a378d20faaafb433b98348c28e
Steven Ufkes <you@example.com>
2017-07-23 11:44:16 -0400

    added in-code test of multiplation.

--- toeplitz_decomp/new_factorize_parrallel.py
+++ toeplitz_decomp/new_factorize_parrallel.py
@@ -342,6 +342,27 @@ class ToeplitzFactorizor:
             if b.rank == s2: # rank s2=k sends to rank 0.
                 s = u1
                 A2 = b.getA2()
+                
+                np.random.seed(42)
+                A_real = np.random.rand(2048,2048)-0.5
+                np.random.seed(43)
+                A_imaginary = 1.0*np.random.rand(2048,2048)-0.5
+                A = A_real+1.0j*A_imaginary
+                del A_real
+                del A_imaginary
+
+                np.random.seed(44)
+                B_real = np.random.rand(2048,2048)-0.5
+                np.random.seed(45)
+                B_imaginary = np.random.rand(2048,2048)-0.5
+                B = B_real+1.0j*B_imaginary
+                del B_real
+                del B_imaginary
+                start = time()
+                    C = np.dot(A,B)
+                end = time()
+                print "Time = "+str(end-start)
+                
                 B2 = A2[s:, :m].dot(np.conj(X2[:p_eff, :m]).T)
                 self.comm.Send(B2, dest=b.getWork2()%self.size, tag=3*num + b.getWork2())
                 del A2
