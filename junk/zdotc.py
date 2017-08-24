a35545b2c95d27787b975de18b78363627999500
Steven Ufkes <you@example.com>
2017-08-24 15:14:25 -0400

    added test of dznrm2.

--- junk/zdotc.py
+++ junk/zdotc.py
@@ -1,6 +1,6 @@
 import sys
 import numpy as np
-from scipy.linalg.blas import zdotc
+from scipy.linalg.blas import zdotc, dznrm2
 import time
 from copy import deepcopy
 
@@ -15,12 +15,6 @@ np.random.seed(43)
 x_imaginary = 1.0*np.random.rand(n)-0.5
 x = x_real+1.0j*x_imaginary
 
-np.random.seed(44)
-y_real = np.random.rand(n)-0.5
-np.random.seed(45)
-y_imaginary = 1.0*np.random.rand(n)-0.5
-y = y_real+1.0j*y_imaginary
-
 indices=range(N) # list to loop over.
 
 #b.getA2().dot(np.conj(X2.T))
@@ -28,24 +22,38 @@ indices=range(N) # list to loop over.
 times_dot = np.empty(N)
 for i in indices:
     start_dot = time.time()
-    C = x.dot(np.conj(y))
+    C = x.dot(np.conj(x))
     end_dot = time.time()
     times_dot[i] = end_dot - start_dot
 C_dot = deepcopy(C)
 
 
-print x.T.flags['F_CONTIGUOUS']
-print y.T.flags['F_CONTIGUOUS']
+#print x.T.flags['F_CONTIGUOUS']
 times_zdotc= np.empty(N)
 for i in indices:
     start_zdotc = time.time()
-    C = zdotc(y.T, x.T)
+    C = zdotc(x.T, x.T)
     end_zdotc = time.time()
     times_zdotc[i] = end_zdotc - start_zdotc
 C_zdotc = deepcopy(C)
 
+times_dznrm2 = np.empty(N)
+for i in indices:
+    start_dznrm2 = time.time()
+    C = dznrm2(x.T)**2
+    end_dznrm2 = time.time()
+    times_dznrm2[i] = end_dznrm2 - start_dznrm2
+C_dznrm2 = deepcopy(C)
 
-print "Equal: "+str(bool(np.allclose(C_dot,C_zdotc)))
+times_dznrm2_np = np.empty(N)
+for i in indices:
+    start_dznrm2_np = time.time()
+    C = np.square(dznrm2(x.T))
+    end_dznrm2_np = time.time()
+    times_dznrm2_np[i] = end_dznrm2_np - start_dznrm2_np
+C_dznrm2_np = deepcopy(C)
+
+print "Equal: "+str(bool(np.allclose(C_dot,C_zdotc) and np.allclose(C_zdotc,C_dznrm2) and np.allclose(C_dznrm2, C_dznrm2_np)))
 print "\ndot():"
 print "min  = "+str(times_dot.min())
 print "max  = "+str(times_dot.max())
@@ -54,3 +62,11 @@ print "\zdotc(A):"
 print "min  = "+str(times_zdotc.min())
 print "max  = "+str(times_zdotc.max())
 print "mean = "+str(times_zdotc.mean())
+print "\dznrm2(A):"
+print "min  = "+str(times_dznrm2.min())
+print "max  = "+str(times_dznrm2.max())
+print "mean = "+str(times_dznrm2.mean())
+print "\dznrm2_np(A):"
+print "min  = "+str(times_dznrm2_np.min())
+print "max  = "+str(times_dznrm2_np.max())
+print "mean = "+str(times_dznrm2_np.mean())
