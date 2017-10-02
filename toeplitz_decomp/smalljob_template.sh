2f3621627dd9cbafca48cbb3bb062807fbdffc07
Steven Ufkes <you@example.com>
2017-10-02 12:55:51 -0400

    Added rob's data.

--- toeplitz_decomp/smalljob_template.sh
+++ toeplitz_decomp/smalljob_template.sh
@@ -11,7 +11,7 @@ module load binutils/2.23 bgqgcc/4.8.1 mpich2/gcc-4.8.1
 method=yty2     # Scheme of decomposition. yty2 is the method described in Nilou's report.
 offsetn=1004
 offsetm=0
-n=1516
+n=512
 m=256
 p=64            # VISAL SAYS: Can set to m/4, m/2, m, 2m. Fastest when set to m/2 or m/4.
 pad=1           # 0 for no padding; 1 for padding.
@@ -19,5 +19,11 @@ pad=1           # 0 for no padding; 1 for padding.
 NP=1024          # Number of MPI processes. Must be set to 2n for this code. NP <= (RPN * bg_size)
 RPN=16          # Number of MPI processes per node = 1,2,4,8,16,32,64. RPN <= NP
 OMP=4           # Number of OpenMP threads per MPI process = 1,2,4,8,16,32,64. (RPN * OMP_NUM_THREADS ) <= 64 = threads per node
+
+if [ "$NP" -ne "$(( 2*n ))" ]
+   then
+   echo "Error: Set the number of MPI processes to 2n. Quitting."
+   exit 1   
+fi
 
 time runjob --np ${NP} --ranks-per-node=${RPN} --envs OMP_NUM_THREADS=${OMP} HOME=$HOME LD_LIBRARY_PATH=/scinet/bgq/Libraries/HDF5-1.8.12/mpich2-gcc4.8.1//lib:/scinet/bgq/Libraries/fftw-3.3.4-gcc4.8.1/lib:$LD_LIBRARY_PATH PYTHONPATH=/scinet/bgq/tools/Python/python2.7.3-20131205/lib/python2.7/site-packages/ : /scratch/s/scinet/nolta/venv-numpy-scipy/bin/python run_real_new.py ${method} ${offsetn} ${offsetm} ${n} ${m} ${p} ${pad}
